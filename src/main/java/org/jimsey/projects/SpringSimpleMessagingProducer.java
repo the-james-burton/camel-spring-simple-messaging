73f9a0d74df607e7f423eb6ca6bdb6aeeba6ba67
the-james-burton <the.james.burton@gmail.com>
2015-10-06 17:57:01 +0100

    license header, format, org imports

--- src/main/java/org/jimsey/projects/SpringSimpleMessagingProducer.java
+++ src/main/java/org/jimsey/projects/SpringSimpleMessagingProducer.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License
+ * Copyright (c) 2015 the-james-burton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.jimsey.projects;
 
 import java.util.Map;
@@ -62,11 +84,11 @@ public class SpringSimpleMessagingProducer extends DefaultProducer {
       return null;
     }
     if (!headers.containsKey(header)) {
-      log.warn("unable to find header '{}'", header);
+      log.trace("unable to find header '{}'", header);
       return null;
     }
     if (headers.get(header) == null) {
-      log.warn("header '{}' is null", header);
+      log.trace("header '{}' is null", header);
       return null;
     }
     return headers.get(header).toString();
